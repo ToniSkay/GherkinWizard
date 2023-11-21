ea2884ef7a7885cc2250460838c3716be3279ea0
a.chuevsky <a.chuevsky@itransition.com>
2023-11-21 21:30:12 +0100

    create aliases and add indexes in common

--- craco.config.ts
+++ craco.config.ts
@@ -1,7 +1,15 @@
 const sassResourcesLoader = require('sass-resources-loader');
 const CracoEsbuildPlugin = require('craco-esbuild');
 
+const path = require('path');
+
 export default {
+  webpack: {
+    alias: {
+      'common-components': path.resolve(__dirname, 'src/common/components'),
+      'utils': path.resolve(__dirname, 'src/common/utils'),
+    },
+  },
   plugins: [
     {
       plugin: CracoEsbuildPlugin,
---
+++ src/common/components/index.ts
@@ -0,0 +1,4 @@
+export * from './Actions/DeleteAction';
+export * from './Header/Header';
+export * from './Loader/Loader';
+export * from './Navigation/Navigation';
