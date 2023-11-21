05adc5d013d6e53be8b78b180d61b7d06f58cc19
Anton Chuevsky <59788580+ToniSkay@users.noreply.github.com>
2023-11-21 21:32:34 +0100

    Refactoring (#4)
    * rework routing
    
    * rework routing
    
    * move layout to Main component
    
    * create aliases and add indexes in common
    
    * create global mixins file
    
    * add .idea to gitignore
    
    ---------
    
    Co-authored-by: a.chuevsky <a.chuevsky@itransition.com>

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -5,15 +5,18 @@ import {MyScenariosPage} from "./pages/MyScenariosPage/MyScenariosPage";
 import {LoginPage} from "./pages/LoginPage/LoginPage";
 import {RegistrationPage} from "./pages/RegistrationPage/RegistrationPage";
 import {ScenarioCreationPage} from "./pages/ScenarioCreationPage/ScenarioCreationPage";
+import {Main} from "./common/components/Main/Main";
 
 export const AppRouter = () => {
     return (
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/all-tasks" element={<MyScenariosPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/registration" element={<RegistrationPage />} />
-            <Route path="/create-scenario" element={<ScenarioCreationPage />} />
-        </Routes>
+            <Routes>
+                <Route path="" element={<Main/>}>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/all-tasks" element={<MyScenariosPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/registration" element={<RegistrationPage />} />
+                    <Route path="/create-scenario" element={<ScenarioCreationPage />} />
+                </Route>
+            </Routes>
     );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root')
@@ -26,9 +25,7 @@ root.render(
             <title></title>
         </head>
 
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <App />
     </>
 );
 
