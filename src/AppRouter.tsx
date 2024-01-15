028e90da50caed9f71081bfd18e9eed38a602409
a.chuevsky <a.chuevsky@itransition.com>
2024-01-15 20:16:41 +0100

    TAS-34 moving and renaming

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -4,7 +4,7 @@ import {HomePage} from "./pages/HomePage/HomePage";
 import {MyScenariosPage} from "./pages/MyScenariosPage/MyScenariosPage";
 import {LoginPage} from "./pages/LoginPage/LoginPage";
 import {RegistrationPage} from "./pages/RegistrationPage/RegistrationPage";
-import {ScenarioCreationPage} from "./pages/ScenarioCreationPage/ScenarioCreationPage";
+import {ScenarioCreateEditPage} from "./pages/ScenarioCreateEditPage/ScenarioCreateEditPage";
 import {Main} from "./common/components/Main/Main";
 
 export const AppRouter = () => {
@@ -15,8 +15,8 @@ export const AppRouter = () => {
                     <Route path="/all-tasks" element={<MyScenariosPage />} />
                     <Route path="/login" element={<LoginPage />} />
                     <Route path="/registration" element={<RegistrationPage />} />
-                    <Route path="/create-scenario" element={<ScenarioCreationPage />} />
-                    <Route path="/edit-scenario/:scenarioId" element={<ScenarioCreationPage />} />
+                    <Route path="/create-scenario" element={<ScenarioCreateEditPage />} />
+                    <Route path="/edit-scenario/:scenarioId" element={<ScenarioCreateEditPage />} />
                 </Route>
             </Routes>
     );
