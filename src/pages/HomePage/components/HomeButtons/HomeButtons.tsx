d31d94535ee25981b3efcaa82f3c2c652602e0aa
a.chuevsky <a.chuevsky@itransition.com>
2024-01-18 14:49:50 +0100

    add ability to navigate to scenario creation and fix styles

--- src/pages/HomePage/components/HomeButtons/HomeButtons.tsx
+++ src/pages/HomePage/components/HomeButtons/HomeButtons.tsx
@@ -1,14 +1,13 @@
-import React from "react";
 import "./HomeButtons.scss";
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 export function HomeButtons() {
   const navigate = useNavigate();
 
   return (
     <div className="home-buttons">
-      <label>Create Task</label>
-      <label onClick={() => navigate("/all-tasks")}>See Your Tasks</label>
+      <label onClick={() => navigate("/create-scenario")}>Create Scenario</label>
+      <label onClick={() => navigate("/all-tasks")}>See My Scenarios</label>
     </div>
   );
 }
